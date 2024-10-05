3e69f10ad1492c7f68582481a32e08a3ed0f6539
campishome <campnapat007@gmail.com>
2024-10-05 14:14:30 +0700

    register0.1

---
+++ lib/pages/RegisterUser.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Registeruser extends StatefulWidget {
+  const Registeruser({super.key});
+
+  @override
+  State<Registeruser> createState() => _RegisteruserState();
+}
+
+class _RegisteruserState extends State<Registeruser> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
