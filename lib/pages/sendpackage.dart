533550c3650992aad3b795eee208f81bb94c1445
jenggod <65011212012@msu.ac.th>
2024-10-08 00:06:51 +0700

    sendpackage0.1

---
+++ lib/pages/sendpackage.dart
@@ -0,0 +1,262 @@
+import 'package:duckddproject/pages/LoginPage.dart';
+import 'package:duckddproject/pages/UserHome.dart';
+import 'package:duckddproject/pages/packagelist.dart';
+import 'package:duckddproject/pages/profile.dart';
+import 'package:flutter/material.dart';
+
+class SendPage extends StatefulWidget {
+  const SendPage({super.key});
+
+  @override
+  State<SendPage> createState() => _SendPageState();
+}
+
+class _SendPageState extends State<SendPage> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: BottomNavigationBar(
+          backgroundColor: const Color.fromARGB(255, 252, 227, 3), // Yellow background
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: 'Profile'),
+            BottomNavigationBarItem(icon: Icon(Icons.logout), label: 'Logout'),
+          ],
+          currentIndex: selectedIndex,
+          selectedItemColor: Colors.black, // Selected item color
+          unselectedItemColor: Colors.black, // Unselected item color
+          onTap: (int index) {
+            if (index == 3) {
+              _showLogoutDialog(context);
+            } else {
+              setState(() {
+                selectedIndex = index;
+                if (selectedIndex == 0) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const UserHomePage()),
+                  );
+                } else if (selectedIndex == 1) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Packagelist()),
+                  );
+                } else if (selectedIndex == 2) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const userProfile()),
+                  );
+                }
+              });
+            }
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              height: 150,
+              width: double.infinity,
+              color: Colors.grey[300], 
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.cloud_upload_outlined,
+                      size: 60,
+                      color: Colors.black45,
+                    ),
+                    onPressed: () {
+                     
+                      print('Cloud upload clicked');
+                    },
+                  ),
+                  const SizedBox(width: 20),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.camera_alt_outlined,
+                      size: 60,
+                      color: Colors.black45,
+                    ),
+                    onPressed: () {
+                      
+                      print('Camera icon clicked');
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 16),
+            
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Package Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+          
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Package Description',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Receiver Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+  
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Receiver Phone Number',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+           Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _showConfirmDialog(context),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  child: const Text(
+                    'Send package',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showLogoutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Logout'),
+          content: const Text('Do you want to logout?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); 
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+              child: const Text('Logout'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showConfirmDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: const Text('Confirm order?'),
+          actions: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green, 
+              ),
+              onPressed: () {
+              Navigator.of(context).pop(); 
+              Confirm(context); 
+            },
+              child: const Text('Confirm'),
+              
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red, 
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+ void Confirm(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        content: SingleChildScrollView( // To avoid overflow
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // Reduce height to fit content
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.of(context).pop(); 
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10), 
+              const Padding(
+                padding: EdgeInsets.only(bottom: 20.0), 
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('COMPLETE SEND ORDER!!!'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
+
+}
